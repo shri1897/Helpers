fb8d3e8cfb5e5b534fc6ef2428617ff05ffe46d3
shri1897 <37364092+shri1897@users.noreply.github.com>
2019-03-14 12:01:50 +0530

    Created DeepCopy function

---
+++ deepCopy.js
@@ -0,0 +1,17 @@
+function deepCopy(element) {
+    if (isArray(element)) {
+        return element.map(el => {
+            return deepCopy(el);
+        });
+    } else if (isDate(element)) {
+        return new Date(element);
+    } else if (isObject(element)) {
+        let newObect = {};
+        for (let key in element) {
+            newObect[key] = deepCopy(element[key])
+        };
+        return newObect;
+    } else {
+        return element
+    }
+}
